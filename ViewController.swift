722665e557d99ff945a85b2afacf6a6b423cf24a
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-10-03 21:52:00 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Jenny Morales on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
