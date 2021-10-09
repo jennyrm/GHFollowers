ebaae64324667e8936854341c4f1b8b287ea18b5
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-10-08 20:17:46 -0700

    created network manager to handle api calls

---
+++ Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Jenny Morales on 10/8/21.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ Screens/FollowerListVC.swift
@@ -0,0 +1,35 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Jenny Morales on 10/6/21.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+
+    }
+
+}//End of class
