9ae029757d5052b8bde6acb180b5d694a73f7ec0
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-10-19 15:27:45 -0700

    created reusable item info views

--- Custom Views/Views/GFItemInfoView.swift
+++ Custom Views/Views/GFItemInfoView.swift
@@ -53,4 +53,23 @@ class GFItemInfoView: UIView {
         ])
     }
     
+    func set(itemInfoType: ItemInfoType, withCount count: Int) {
+        switch itemInfoType {
+        case .repos:
+            symbolImageView.image = UIImage(systemName: SFSymbols.repos)
+            titleLabel.text = "Public Repos"
+        case .gists:
+            symbolImageView.image = UIImage(systemName: SFSymbols.gists)
+            titleLabel.text = "Public Gists"
+        case .followers:
+            symbolImageView.image = UIImage(systemName: SFSymbols.followers)
+            titleLabel.text = "Public Followers"
+        case .following:
+            symbolImageView.image = UIImage(systemName: SFSymbols.following)
+            titleLabel.text = "Public Following"
+        }
+        
+        countLabel.text = String(count)
+    }
+    
 }//End of class
