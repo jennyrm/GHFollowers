262a3c339be81dbf67135215c9c551167eb0946d
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-10-20 12:57:57 -0700

    added children itemInfoVCs to containerVC

---
+++ Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Jenny Morales on 10/20/21.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+
+}//End of class
