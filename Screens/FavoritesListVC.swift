b2fd29e2b97f1f63b797ef20c0f52cc489010d6f
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-10-29 18:27:36 -0700

    working on delete functionality

--- Screens/FavoritesListVC.swift
+++ Screens/FavoritesListVC.swift
@@ -89,9 +89,9 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
         
-        let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
+//        let favorite = favorites[indexPath.row]
+//        favorites.remove(at: indexPath.row)
+//        tableView.deleteRows(at: [indexPath], with: .fade)
         
         PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
             guard let self = self else { return }
