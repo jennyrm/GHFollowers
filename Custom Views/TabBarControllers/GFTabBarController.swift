3669cb561a4e232700887598cbee9a18c39cde06
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2022-01-06 23:54:17 -0800

    working on code optimization

---
+++ Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Jenny Morales on 1/6/22.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
